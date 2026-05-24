6794786884594198c96d5e5a71230625ae485d11
srilatha2012 <srilathp2012@gmail.com>
2026-05-23 21:54:39 -0400

    Added User reference to Note Schema

---
+++ models/Note.js
@@ -0,0 +1,30 @@
+//Import mongoose package
+const mongoose = require("mongoose");
+
+//define Note schema
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    content:{
+        type: String,
+        required: true,
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+});
+
+//Create Note model from noteSchema
+const Note = mongoose.model("Note", noteSchema);
+
+module.exports = Note;
